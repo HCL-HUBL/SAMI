efc72ac925c01d0a3c9a20044babe749561bd78a
Ext-Valentin WUCHER01 <wucherex@chu-lyon.fr@ge91098-vm1.chu-lyon.fr>
2022-10-11 10:20:08 +0200

    working version

--- scripts/umi_table.R
+++ scripts/umi_table.R
@@ -8,25 +8,21 @@ lines <- c(
   "    umi_stats:",
   "        plot_type: 'generalstats'",
   "        pconfig:",
-  "            - Mean:",
+  "            - UMI.mean:",
   "                namespace: 'UMI'",
   "                description: 'Mean number of UMIs per sequence'",
   "                format: '{:,.0f}'",
-  "            - Median:",
+  "            - UMI.median:",
   "                namespace: 'UMI'",
   "                description: 'Median number of UMIs per sequence'",
   "                format: '{:,.0f}'",
-  "            - Standard deviation:",
-  "                namespace: 'UMI'",
-  "                description: 'Standard deviation of the number of UMIs per sequence'",
-  "                format: '{:,.0f}'",
-  "            - Min:",
-  "                namespace: 'UMI'",
-  "                description: 'Minimum number of UMIs per sequence'",
-  "                format: '{:,.0f}'",
-  "            - Max:",
+  "            - UMI.max:",
   "                namespace: 'UMI'",
   "                description: 'Maximum number of UMIs per sequence'",
+  "                format: '{:,.0f}'",
+  "            - UMI.unique:",
+  "                namespace: 'UMI'",
+  "                description: 'Proportion of sequences with one read per UMI'",
   "                format: '{:,.0f}'",
   "        data:"
 )
@@ -39,11 +35,10 @@ for(ifile in allfile)
   umi_hist <- read.delim(file=ifile)
   tostat <- rep(x=umi_hist$family_size, times=umi_hist$count)
   lines <- c(sprintf("            '%s':
-                Mean: %f
-                Median: %f
-                Standard deviation: %f
-                Min: %i
-                Max: %i", samp, mean(tostat), median(tostat), sd(tostat), min(tostat), max(tostat))
+                UMI.mean: %f
+                UMI.median: %f
+                UMI.max: %f,
+                UMI.unique: %f", samp, mean(tostat), median(tostat), max(tostat), umi_hist$fraction[1])
 )
   cat(lines, sep="\n", file="./umi_table_mqc.yaml", append=TRUE)
 }
